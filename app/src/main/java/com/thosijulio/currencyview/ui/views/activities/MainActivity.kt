171e17e0182b35ec31f8bfcabed15cd4297e3be7
thosijulio <thosijulio@gmail.com>
2024-06-24 10:57:26 -0300

    config inicial do projeto

---
+++ app/src/main/java/com/thosijulio/currencyview/ui/views/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.thosijulio.currencyview.ui.views.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.thosijulio.currencyview.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
